a6a9159714fda319dc52573ae3352602f073875b
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-11 11:50:05 +0800

    sw
    sw

--- sw/main.swift
+++ sw/main.swift
@@ -1,12 +1,43 @@
-//
-//  main.swift
-//  sw
-//
-//  Created by s20181102929 on 2019/9/11.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+var index = 1
+var sum = 0
+while index<=100
+{
+    sum = sum + index
+    index = index + 1
+}
+print("使用while循环的值是\(sum)")
+var sumfor = 0
+for x in 1...100
+{
+    sumfor = sumfor + x
+}
+print("使用for循环的值是\(sumfor)")
+var sumrepeat = 0
+var j = 1
+repeat
+{
+    sumrepeat = sumrepeat + j
+    j = j + 1;
+}while(j<=100)
+print("使用repeata循环的值是\(sumrepeat)")
+/*新的*/
+var input = readLine()!
+while input != "q"
+{
+    var change = Int(input)!
+    print(2*change)
+    input = readLine()!
+}
+/*xin*/
+var input1 = readLine()!
+var max = 0
+while input1 != "end"
+{
+    var n = Int(input1)!
+    if n>max
+    {
+        max = n
+    }
+    input1 = readLine()!
+}
+print(max)
